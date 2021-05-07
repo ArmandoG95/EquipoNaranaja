8eb51f610431c062dcf803cb30414006cfc13c27
Armando Gonzalez Hernandez <sixmit@hotmail.com>
2021-05-07 13:00:41 -0400

    Agregue forma Advisor.

---
+++ EquipoNaranja/EquipoNaranja/EquipoNaranja/FrmAdvisor.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace EquipoNaranja
+{
+    partial class FrmAdvisor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmAdvisor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 414);
+            this.Name = "FrmAdvisor";
+            this.Text = "FrmAdvisor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
